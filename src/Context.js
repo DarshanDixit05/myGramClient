e0a1ef509c96273956622ffe3139a88f6ca5e90b
DarshanDixit05 <darshandixit0@gmail.com>
2023-12-10 22:20:57 +0530

    Cleanup :(

--- src/Context.js
+++ src/Context.js
@@ -2,7 +2,9 @@ import {createContext, useState} from 'react'
 
 export const Id = createContext();
 
-export const Context = ({children}) => {
+const Context = ({children}) => {
   const [id, setId] = useState();
   return <Id.Provider value={{id, setId}}>{children}</Id.Provider>
 }
+
+export default Context;
