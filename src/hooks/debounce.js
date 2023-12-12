38c1316d11f7ace2396be5fc2fcbbe3b4b1385a3
DarshanDixit05 <darshandixit0@gmail.com>
2023-12-12 19:17:35 +0530

    debounce hook added

---
+++ src/hooks/debounce.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from 'react'
+
+const useDebounce = (value, delay) => {
+    const [debounceVal, setDebounceVal] = useState()
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebounceVal(value);
+        }, delay);
+
+        return () =>{
+            clearTimeout(handler);
+        };
+    }, [value, delay])
+
+    return debounceVal;
+}
+
+export default useDebounce;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,23 +1,38 @@
-import React, {useContext} from 'react'
+import useDebounce from "../hooks/debounce.js";
+import React, {useContext, useState, useEffect} from 'react'
 import SimpleSidebar from "../components/SimpleSidebar.js";
 import Post from "../components/Post.js";
 import {IdContext} from "../Context.js";
+import { Input } from '@chakra-ui/react'
+import axios from 'axios';
+const BASE_URL="http://localhost:1999/api/v1"
 
 function Main() {
-  const {id, setId} = useContext(IdContext)
+  const {id, setId} = useContext(IdContext);
+  const [search, setSearch] = useState("");
+
+  const debounceSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    const f = async()=>{
+      const response = await axios.get(BASE_URL+"/searchUser", {debounceSearch});
+
+      console.log(response);
+    }
+    if(debounceSearch)f();
+    
+  }, [debounceSearch])
+
   return (
     <div id="main_container">
       <SimpleSidebar />
       <div id="main_right_container">
-        <div id="main_story_section">
-          Stories
+        <div id="main_searcn_section">
+          <Input id="search_bar" placeholder='Search User' onChange={(e)=>setSearch(e.target.value)}/>
         </div>
         <div id="right-feed-section">
           <div id="content-section">
             <Post />
-          </div>
-          <div id="suggestion-section">
-            Suggestion
           </div>
         </div>
       </div>
